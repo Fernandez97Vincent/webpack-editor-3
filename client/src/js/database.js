55cf4a7de0c94358e8e3f989028cddebf491fb45
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-10-31 23:02:58 -0700

    Finished database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Grabbing data from JATE')
+  //connect
+  const jateData = await openDB('jate', 1);
+
+  const tx = jateData.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const req = store.put({id: 1, value: content});
+
+  const res = await req;
+
+  console.log('Data has been saved to JATE', res.value);
+  console.error('putDb not implemented');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (id, content) => {
+  console.log('Request to update JATE');
+
+  const jateData = await openDB('jate', 1);
+
+  const tx = jateData.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const req = store.get(1);
+
+  const res = await req;
+  console.log('Data has been saved to JATE', res.value);
+  return result?.value
+}
 
 initdb();
